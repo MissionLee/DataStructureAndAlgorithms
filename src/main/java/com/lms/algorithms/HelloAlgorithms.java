e4c1cbdbbb054f5574dcf4939b5fc57c71877945
MissionLee <MissionLee@163.com>
2018-04-11 19:15:56 +0800

    hello algorithms

---
+++ src/main/java/com/lms/algorithms/HelloAlgorithms.java
@@ -0,0 +1,13 @@
+package com.lms.algorithms;
+
+/**
+ * @author: MissingLi
+ * @date: 11/04/18 19:13
+ * @Description:
+ * @Modified by:
+ */
+public class HelloAlgorithms {
+    public static void main(String[] args) {
+        System.out.println("Hello Algorithms");
+    }
+}
