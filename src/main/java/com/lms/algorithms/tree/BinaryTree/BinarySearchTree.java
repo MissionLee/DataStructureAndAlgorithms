e7903fe96d16d8ecd5b969d5a850669be31c682d
MissionLee <MissionLee@163.com>
2018-04-14 18:05:13 +0800

    new class com.lms.algorithms.tree.BinaryTree.BinarySearchTreeAVL

--- src/main/java/com/lms/algorithms/tree/BinaryTree/BinarySearchTree.java
+++ src/main/java/com/lms/algorithms/tree/BinaryTree/BinarySearchTree.java
@@ -1,203 +1,272 @@
 package com.lms.algorithms.tree.BinaryTree;
 
-import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+
 
 /**
  * @author: book : Data Structure and Algorithms analysis in Java
  */
 public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
-    private static class BinaryNode<AnyType>{
-        BinaryNode(AnyType theElement){this(theElement,null,null);}
-
-        public BinaryNode(AnyType theElement,BinaryNode<AnyType> lt,BinaryNode<AnyType> rt) {
-            element=theElement;
-            left=lt;
-            right=rt;
+    private static class BinaryNode<AnyType> {
+        BinaryNode(AnyType theElement) {
+            this(theElement, null, null);
         }
+
+        public BinaryNode(AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+        }
+
         AnyType element;
         BinaryNode<AnyType> left;
         BinaryNode<AnyType> right;
     }
 
     private BinaryNode<AnyType> root;
+
     public BinarySearchTree() {
         root = null;
     }
 
-    public void makrEmpty(){
-        root =null;
+    public void makrEmpty() {
+        root = null;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return root == null;
     }
 
-    public boolean contains(AnyType x){
-        return  contains(x,root);
+    public boolean contains(AnyType x) {
+        return contains(x, root);
     }
+
     /**
      * 1.如果 被比较的节点为空 返回 false（空节点 也不会有其他的 left right 节点了）
      * 2.根据比较结果大小，不断的向左右 查找 直到结束 或 找到
-    */
-    public boolean contains(AnyType x,BinaryNode<AnyType> t){
-        if (t == null) return  false;
+     */
+    public boolean contains(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null) return false;
         int compareResult = x.compareTo(t.element);
 
-        if(compareResult>0){
-            return contains(x,t.right);
-        }else if(compareResult<0){
-            return contains(x,t.left);
-        }else{
+        if (compareResult > 0) {
+            return contains(x, t.right);
+        } else if (compareResult < 0) {
+            return contains(x, t.left);
+        } else {
             return true;
         }
     }
 
-    public AnyType findMin(){
-        return  findMin(root.element,root.left);
+    // 找最值的时候，我们很自然的会想到一种方法： 递归
+    //  递归确实非常好用，但是递归也的效率毕竟有制约
+    //  在 书中 看到了 用递归和 用循环 两种方式来完成 找最值的方法
+    // 我再这里 也在不同的实现里面尝试一下
+    public AnyType findMinValue() {
+        return findMinValue(root.element, root.left);
     }
-    public AnyType findMin(AnyType cur,BinaryNode<AnyType> t){
-        if(t==null){
+
+    public AnyType findMinValue(AnyType cur, BinaryNode<AnyType> t) {
+        if (t == null) {
             return cur;
-        }else{
-            return findMin(t.element,t.left);
+        } else {
+            return findMinValue(t.element, t.left);
         }
 
     }
-    public AnyType findMax(){
-        return findMax(root.element,root.right);
+
+    private BinaryNode<AnyType> findMinNode() {
+        return findMinNode(root);
     }
-    public AnyType findMax(AnyType cur,BinaryNode<AnyType> t){
-        if(t.right ==null){
+
+    private BinaryNode<AnyType> findMinNode(BinaryNode<AnyType> t) {
+        while (t.left != null) {
+            t = t.left;
+        }
+        return t;
+    }
+
+    public AnyType findMaxValue() {
+        return findMaxValue(root.element, root.right);
+    }
+
+    public AnyType findMaxValue(AnyType cur, BinaryNode<AnyType> t) {
+        if (t.right == null) {
             return cur;
-        }else{
-            return findMax(cur,t.right);
+        } else {
+            return findMaxValue(cur, t.right);
         }
     }
-    public void insert(AnyType x){
-        if(root ==null){
+
+    public void insert(AnyType x) {
+        if (root == null) {
             root = new BinaryNode<AnyType>(x);
+            System.out.println(root);
+            System.out.println(root.element);
             return;
-        }else{
-            insert(x,root);
+        } else {
+            System.out.println("insert" + x);
+            insert(x, root);
+
         }
     }
-    public void insert(AnyType x,BinaryNode<AnyType> t){
-        if(t == null){
-            t = new BinaryNode<AnyType>(x);
-//            System.out.println(t);
-            return;
-        }else{
-            int compareResult = x.compareTo(t.element);
-            if(compareResult>0){
-                if(t.right==null){
-                    t.right= new BinaryNode<AnyType>(x);
-                    return;
-                }
-                insert(x,t.right);
-            }else{
-                if(t.left==null){
-                    t.left = new BinaryNode<AnyType>(x);
-                    return;
-                }
-                insert(x,t.left);
+
+    public void insert(AnyType x, BinaryNode<AnyType> t) {
+
+        int compareResult = x.compareTo(t.element);
+        if (compareResult > 0) {
+            if (t.right == null) {
+                t.right = new BinaryNode<AnyType>(x);
+            } else {
+                insert(x, t.right);
+            }
+        } else {
+            if (t.left == null) {
+                t.left = new BinaryNode<AnyType>(x);
+            } else {
+                insert(x, t.left);
             }
         }
+
     }
-    public void remove(AnyType x){
-        remove(x,root);
+
+    public void remove(AnyType x) {
+        remove(x, root);
     }
-    public BinaryNode<AnyType> remove(AnyType x,BinaryNode<AnyType> t){
+
+    public BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
         //对于节点t 进行处理，返回 处理过后的t（删除指定元素后的t）
         // 我们的思路就是： 如果t空的，直接返回
         //                如果 指定元素可能在 左边，那么让左边 = 新的左边
         //                如果 指定元素可能在 右边，那么让右边 = 新的右边
         //              而，新的左边，右边，就是 递归得到的
-        if(t ==null) return t;
+        if (t == null) return t;
         int compareResult = x.compareTo(t.element);
-        if(compareResult==0){//如果 当前的t就是 就是目标
+        if (compareResult == 0) {//如果 当前的t就是 就是目标
             System.out.println("find the number,now delete it");
-            
-            if(t.left!=null&&t.right!=null){//如果 左右都有节点
+
+            if (t.left != null && t.right != null) {//如果 左右都有节点
                 System.out.println("the node has two children node");
-                t.element=findMin(t.element,t.right);//那么当前值替换为右侧子树中最小值
-                t.right=remove(t.element,t.right);//右侧变为 右侧去掉最小值后新的子树
-            }else  if(t.left==null&&t.right==null){
+                t.element = findMinValue(t.element, t.right);//那么当前值替换为右侧子树中最小值
+                t.right = remove(t.element, t.right);//右侧变为 右侧去掉最小值后新的子树
+            } else if (t.left == null && t.right == null) {
                 //如果当前没有子节点了
-                t=null;
-            }else{
-               return t=t.left==null?t.right:t.left;//在只有一个子节点的时候，用子节点替换当前节点
+                t = null;
+            } else {
+                return t = t.left == null ? t.right : t.left;//在只有一个子节点的时候，用子节点替换当前节点
             }
-        }else if(compareResult>0){
-            t.right = remove(x,t.left);
-        }else{
-            t.left = remove(x,t.left);
+        } else if (compareResult > 0) {
+            t.right = remove(x, t.left);
+        } else {
+            t.left = remove(x, t.left);
         }
 
         return t;
-//        BinaryNode<AnyType> theNode = find(x,t);
-//        System.out.println(theNode);
-//        if(theNode != null){
-//            if(theNode.left!=null){
-//                System.out.println(" the node  =  the node .left");
-//                theNode=theNode.left;
-//                return;
-//            }else if(theNode.right!=null){
-//                System.out.println("the node = the node .right");
-//                theNode=theNode.right;
-//
-//                return;
-//            }else{
-//                System.out.println("make the node = null");
-//                theNode=null;
-//                System.out.println(theNode);
-//                return;
-//            }
-//        }
+
     }
-    private BinaryNode<AnyType> find(AnyType x,BinaryNode<AnyType> t){
-        if(t==null ){
+
+    private BinaryNode<AnyType> find(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null) {
             return null;
-        }else{
+        } else {
             int compareReuslt = x.compareTo(t.element);
-            if(compareReuslt==0) {
-                System.out.println(" find the number "+x.toString());
+            if (compareReuslt == 0) {
+                System.out.println(" find the number " + x.toString());
                 System.out.println(t);
                 return t;
-            }else if(compareReuslt>0){
-                return find(x,t.right);
-            }else{
-                return find(x,t.left);
+            } else if (compareReuslt > 0) {
+                return find(x, t.right);
+            } else {
+                return find(x, t.left);
             }
         }
     }
-    public void printTree(){
+    //printTree 实际上就是一次对整个树的遍历
+    //我在看书的时候看到 对于树的几种遍历
+    //  前序遍历：根节点->左子树->右子树（根节点在前面）
 
+    //中序遍历：左子树->根节点->右子树（根节点在中间）
+
+    //后序遍历：左子树->右子树->根节点（根节点在后边）
+
+    //  递归的 -> 与树的结构对应，所以比较容易实现
+    //  非递归的 ->
+    public void printTreePreorderTraversal() {
+
+        printTreePreorderTraversal(root);
     }
-    public int depth(){
+
+    public void printTreePreorderTraversal(BinaryNode<AnyType> t) {
+        // recursive
+        // the Preorder Traversal is normally used  in a Search condition
+        if (t == null) return;
+        System.out.println(t.element);
+        if (t.left != null) {
+            printTreePreorderTraversal(t.left);
+
+        }
+        if (t.right != null) {
+            printTreePreorderTraversal(t.right);
+        }
+    }
+
+    public void printTreeInorderTraversal() {
+        printTreeInorderTraversal(root);
+    }
+
+    public void printTreeInorderTraversal(BinaryNode<AnyType> t) {
+        // non-recursive
+        // the output will be in order
+        Stack<BinaryNode> s = new Stack<BinaryNode>();
+        while (t != null || !s.empty()) {
+            while (t != null) {
+                s.push(t);
+                t = t.left;
+            }
+            if (!s.empty()) {
+                t = s.pop();
+                System.out.println(t.element);
+                t = t.right;
+            }
+        }
+    }
+
+    public void printTreePostorderTraversal() {
+        // non-recursive
+    }
+
+    public int depth() {
         return 0;
     }
-    @Test
-    public void test01(){
+
+
+    public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree<Integer>();
+        bst.insert(2);
         bst.insert(1);
-        bst.insert(11);
-        bst.insert(12);
-        bst.insert(163);
-        bst.insert(134);
-        bst.insert(541);
-        bst.insert(14);
-        bst.insert(12);
-        bst.insert(10);
-        bst.insert(91);
-//        System.out.println(bst.root.right.element);
-//        System.out.println(bst.root);
-//        System.out.println(bst.isEmpty());
-        System.out.println(bst.contains(91));
-        System.out.println(bst.contains(11));
-        bst.remove(11);
+        bst.insert(3);
+        bst.insert(45);
+        bst.insert(35);
+        bst.insert(42345);
+        bst.insert(374);
+        bst.insert(2436);
+        bst.insert(34367);
+        bst.insert(45791);
+        bst.insert(34414);
+        bst.insert(8);
+        bst.insert(55);
+
         for (int i = 0; i < 100; i++) {
 
         }
         System.out.println(bst.contains(11));
+        System.out.println(bst.root);
+        System.out.println(bst.root.right);
+        System.out.println("-----------------------------------");
+        bst.printTreePreorderTraversal();
+        System.out.println("----------------------");
+        bst.printTreeInorderTraversal();
     }
 }
---
+++ src/main/java/com/lms/algorithms/tree/BinaryTree/BinarySearchTreeAVL.java
@@ -0,0 +1,217 @@
+package com.lms.algorithms.tree.BinaryTree;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+/**
+ * @author: MissingLi
+ * @date: 14/04/18 10:23
+ * @Description:
+ * @Modified by:
+ */
+public class BinarySearchTreeAVL<AnyType extends Comparable<? super AnyType>> {
+    // Allowed imbalance
+    private static final int ALLOWED_IMBALANCE = 1;
+
+    private static class AvlNode<AnyType> {
+        AvlNode(AnyType e) {
+            this(e, null, null);
+        }
+
+        AvlNode(AnyType e, AvlNode<AnyType> lt, AvlNode<AnyType> rt) {
+            element = e;
+            left = lt;
+            right = rt;
+            height = 0;
+        }
+
+        int depu = 1;
+        AnyType element;
+        AvlNode<AnyType> left;
+        AvlNode<AnyType> right;
+        int height;
+    }
+
+    public BinarySearchTreeAVL() {
+
+    }
+
+    private AvlNode root;
+
+
+    private int height(AvlNode<AnyType> t) {
+        return t == null ? -1 : t.height;
+    }
+
+    public void insert(AnyType x) {
+        root = insert(x,root);
+    }
+//    private void insert(AnyType x, AvlNode<AnyType> t){
+//
+//    }
+
+    private AvlNode<AnyType> insert(AnyType x, AvlNode<AnyType> t) {
+//        System.out.println("insert");
+        //如果 t为空，在此处创建一个 节点
+        //否则用 compareTo 判断将 insert操作 交接给 这个节点的左右节点
+        if (t == null) {
+//            System.out.println("t == null");
+            t = new AvlNode<AnyType>(x, null, null);
+        } else {
+            int compareResult = x.compareTo(t.element);
+            if (compareResult > 0) {
+                t.right = insert(x, t.right);
+
+//                t.height=Math.max(height(t.left),height(t.right));
+            } else if (compareResult < 0) {
+                t.left = insert(x, t.left);
+//                t.height=Math.max(height(t.left),height(t.right));
+            } else {
+                t.depu++;
+            }
+        }
+        return balance(t);
+    }
+
+    private AvlNode<AnyType> balance(AvlNode<AnyType> t) {
+        //要求：  1.维护每个节点的高度 height ： 每一次递归返回，所在节点都是 两个子节点最高高度+1
+        //       2.维持平衡
+
+        //对于一个节点 判断是否平衡，首先认为 在当次 insert 之前是平衡的
+        // 然后 造成不平衡
+        // 1. 对于叶子节点 没啥影响 因为左右都是 null
+        // 2. 对于其他节点，每次判定是否平衡，并且进行转换
+        if(t==null){
+            return t;
+        }
+        // s1 : 左树偏高
+        if(height(t.left)-height(t.right)>ALLOWED_IMBALANCE){
+            // 左左偏高
+            if(height(t.left.left)>=height(t.left.right))
+                t=rotateWithLeftChild(t);
+            // 左右偏高
+            else
+                t=doubleWithLeftChild(t);
+        }else if(height(t.right)-height(t.left)>ALLOWED_IMBALANCE){
+            if(height(t.right.right)>=height(t.right.left))
+                t=rotateWithRightChild(t);
+            else
+                t=doubleWithRightChild(t);
+        }
+        t.height = Math.max(height(t.left),height(t.right))+1;
+        return t;
+    }
+    // 关于旋转，原理分析请参考 Data Sturcture and Algorithm Analysis in Java书中的讲解
+    // 这里 是我的总结 : 
+    //       [这里的左旋，右旋就是 书中的左旋和右旋。下面几个方法中的左右，指的是 左偏高，右偏高]
+
+    // 左左 ： 单-右旋转
+    // 右右 ： 单-左旋转
+    // 左右 ： 右旋（深层）+左旋
+    // 右左 ： 左旋（深层）+右旋
+    private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2){
+        //右旋转
+
+        // 当前元素 左左偏高
+        // 1. t.left 转为t
+        // 2. t 转为 t.right
+        // 3. t.left.left 转为 t.left
+        //               O(k2)       U(k1)
+        //            U(k1) O =>   U    O(k2)
+        //          U   K              K  O
+        AvlNode<AnyType> k1=k2.left;
+        k2.left=k1.right;
+        k1.right=k2;
+        k2.height=Math.max(height(k2.left),height(k2.right))+1;
+        k1.height=Math.max(height(k1.left),height(k1.right))+1;
+        return k1;
+
+        //下方这一部分代码 是我最初的想法，其中有一些问题
+        // 1. 忘了更新高度
+        // 2. 按照我这个写法，是没法兼容 容许2层以及以上的高度差距的 
+//        AvlNode<AnyType> newt = new AvlNode<AnyType>(t.left.element);
+//        newt.right=t;
+//        newt.left=t.left.left;
+//        return newt;
+    }
+    private AvlNode<AnyType> doubleWithLeftChild(AvlNode<AnyType> k3){
+        k3.left=rotateWithRightChild(k3.left);
+        return rotateWithLeftChild(k3);
+    }
+    private AvlNode<AnyType> rotateWithRightChild(AvlNode<AnyType> k2){
+        // 左旋转
+        
+        //        O(k2)              U(k1)
+        //      O    U(k1)  =>    O(k2) U
+        //         K   U        O   K
+        AvlNode<AnyType> k1 =k2.right;
+        k2.right=k1.left;
+        k1.left=k2;
+        k2.height=Math.max(height(k2.left),height(k2.right))+1;
+        k1.height=Math.max(height(k1.left),height(k1.right))+1;
+        return k1;
+    }
+    private AvlNode<AnyType> doubleWithRightChild(AvlNode<AnyType> k3){
+        k3.right=rotateWithLeftChild(k3.right);
+        return  rotateWithRightChild(k3);
+    }
+
+    public void printTreeInorderTraversal() {
+        printTreeInorderTraversal(root);
+    }
+
+    public void printTreeInorderTraversal(AvlNode<AnyType> t) {
+        // non-recursive
+        // the output will be in order
+        Stack<AvlNode> s = new Stack<AvlNode>();
+        while (t != null || !s.empty()) {
+            while (t != null) {
+                s.push(t);
+                t = t.left;
+            }
+            if (!s.empty()) {
+                t = s.pop();
+                System.out.println(t.element);
+                t = t.right;
+            }
+        }
+    }
+
+
+
+    @Test
+    public void TestInsert() {
+        BinarySearchTreeAVL binarySearchTreeAVL = new BinarySearchTreeAVL<Integer>();
+        binarySearchTreeAVL.insert(2);
+        binarySearchTreeAVL.insert(1);
+        binarySearchTreeAVL.insert(3);
+        binarySearchTreeAVL.insert(3);
+        binarySearchTreeAVL.insert(31);
+        binarySearchTreeAVL.insert(32);
+        binarySearchTreeAVL.insert(13);
+        binarySearchTreeAVL.insert(1233);
+        binarySearchTreeAVL.insert(35);
+        binarySearchTreeAVL.insert(36);
+        binarySearchTreeAVL.insert(8234);
+        binarySearchTreeAVL.insert(0);
+
+        System.out.println(binarySearchTreeAVL.root.right.depu);
+        System.out.println(binarySearchTreeAVL.root.height);
+//        binarySearchTreeAVL.printTreeInorderTraversal();
+    }
+    @Test
+    public void TestHeight(){
+        BinarySearchTreeAVL ba2 = new BinarySearchTreeAVL<Integer>();
+        for (int i = 0; i < 10000; i++) {
+            ba2.insert(Math.floor(Math.random()*1000)-500);
+            if(i%1000==0){
+                System.out.println(ba2.root.element);
+                System.out.println(ba2.root.height);
+            }
+        }
+
+//        ba2.printTreeInorderTraversal();
+    }
+}
